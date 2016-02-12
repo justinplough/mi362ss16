f5f8cd76e8a9df9fa73610071e764b7fe68e33ed
justinplough <ploughj@msu.edu>
2016-02-11 21:54:18 -0500

    updated

--- week4/server.js
+++ week4/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 http.createServer( (request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end(<h1>'Hello World\n'</h1>);
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('<html><body><h1>Hello, World!</h1></body></html>');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
