2c0b0249174d79a02bf517cf55ac24de13433df4
justinplough <ploughj@msu.edu>
2016-02-11 22:04:49 -0500

    with paragraph

--- week4/server.js
+++ week4/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer( (request, response) => {
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('<html><body><h1>Hello, World!</h1></body></html>');
+  response.end('<html><body><h1>Hello, World!\n</h1><p>This is a node app!</p></body></html>');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
